1ab4338178429e40fdc3984b160eb6ed610dea19
Thiago Cândido Rocha <thiagorocha192000@gmail.com>
2021-09-06 09:28:18 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,19 +5,11 @@ int main()
     FILE *fp;
     Node *raiz=NULL;
     char palavra[30];
-    int i;
     fp=fopen("Dicionario//Dictionary.txt","r");
-    while(fgets(palavra,30,fp)){
-        for(i=0;i<(int)strlen(palavra);i++){
-            if(palavra[i]=='\n'){
-                palavra[i]='\0';
-            }
-        }
-        insere(&raiz,&palavra[0]); 
-    }
-    printf("TRAVERSAL:\n");
+    Dicionario(&raiz,fp,&palavra[0]);
+    /*printf("TRAVERSAL:\n");
     percorre(raiz);
     printf("\n\nPESQUISA:");
-    search(raiz,"luffy")? printf("ENCONTROU\n"):printf("NAO ENCONTROU\n");
+    search(raiz,"a")? printf("ENCONTROU\n"):printf("NAO ENCONTROU\n");  -> Apenas para testar a TST*/
     return 0;
 }
